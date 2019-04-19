80b91f4fbd217b66a2d21b53d94596786b9096e5
Terry Ewing on jumpbox1 <tewing@jumpbox1>
2019-04-19 00:43:34 +0000

    delete before create

--- tools/create_maas_node_scripts.sh
+++ tools/create_maas_node_scripts.sh
@@ -4,6 +4,8 @@
 
 cd ~/maas-scripts/maas-node-scripts
 for F in *; do
+	echo maas opsmtn node-script delete script@=./$F
+	maas opsmtn node-script delete script@=./$F
 	echo maas opsmtn node-scripts create script@=./$F
 	maas opsmtn node-scripts create script@=./$F
 done
--- tools/update_maas_node_scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# update all the scripts with the ones in the repo
-
-cd ~/maas-scripts/maas-node-scripts
-for F in *; do
-   echo maas opsmtn node-script update $F script@=$F
-   maas opsmtn node-script update $F script@=$F 
-done
